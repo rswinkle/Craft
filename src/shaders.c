a488603ab514fcf1702d10b4d9d88bc706304163
Robert Winkler <rob121618@gmail.com>
2020-11-14 01:05:13 -0700

    Start porting shaders to PortableGL

---
+++ src/shaders.c
@@ -0,0 +1,141 @@
+
+void passthrough_vs(float* vs_output, void* vertex_attribs, Shader_Builtins* builtins, void* uniforms);
+void point_sprites_fs(float* fs_input, Shader_Builtins* builtins, void* uniforms);
+
+
+typedef struct My_Uniforms
+{
+	mat4 matrix;
+
+	vec3 camera;
+	float fog_distance;
+	int ortho;
+	GLuint tex;
+
+	vec3 light_dir;
+} My_Uniforms;
+
+
+// Could/should just use the default shaders in PortableGL for lines
+void line_vp(float* vs_output, void* vertex_attribs, Shader_Builtins* builtins, void* uniforms)
+{
+	builtins->gl_Position = mult_mat4_vec4(*((mat4*)uniforms), ((vec4*)vertex_attribs)[0]);
+}
+
+void line_fs(float* fs_input, Shader_Builtins* builtins, void* uniforms)
+{
+	vec4* fragcolor = &builtins->gl_FragColor;
+	//wish I could use a compound literal, stupid C++ compatibility
+	fragcolor->x = 0.0f;
+	fragcolor->y = 0.0f;
+	fragcolor->z = 0.0f;
+	fragcolor->w = 1.0f;
+}
+
+
+/*
+
+   The order specified is the order stored
+incoming attributes:
+	vec4 position
+	vec3 normal
+	vec4 uv
+
+outgoing parameters: (varying == smooth)
+	varying vec2 fragment_uv;
+	varying float fragment_ao;
+	varying float fragment_light;
+	varying float fog_factor;
+	varying float fog_height;
+	varying float diffuse;
+
+
+*/
+void block_vs(float* vs_output, void* vertex_attribs, Shader_Builtins* builtins, void* uniforms)
+{
+	//convenience
+	vec4* v_attribs = vertex_attribs;
+	float* vs_out = (vec3*)vs_output;;
+	My_Uniforms* u = uniforms;
+
+	builtins->gl_Position = mult_mat4_vec4(u->matrix, v_attribs[0]);
+
+	// outputs, to assign to vs_out
+	vec2 fragment_uv = *(vec2*)&v_attribs[1];
+	*(vec2*)vs_out = fragment_uv;
+
+	float fragment_ao = 0.3 + (1.0 - uv.z) * 0.7;
+	vs_out[2] = fragment_ao;
+	float fragment_light = uv.w;
+	vs_out[3] = fragment_light;
+
+	const light_direction = normalize(vec3(-1.0, 1.0, -1.0));
+	float tmp = dot(normal, light_direction);  // avoid macro duplication
+	float diffuse = MAX(0.0, tmp);
+	vs_out[6] = diffuse;
+
+	if (u->ortho) {
+		fog_factor = 0.0;
+		fog_height = 0.0;
+	} else {
+		float camera_distance = distance(camera, vec3(position));
+		fog_factor = pow(clamp(camera_distance / fog_distance, 0.0, 1.0), 4.0);
+		float dy = position.y - camera.y;
+		// TODO
+		float dx = distance(position.xz, camera.xz);
+		fog_height = (atan(dy, dx) + RM_PI / 2) / RM_PI;
+	}
+	vs_out[4] = fog_factor;
+	vs_out[5] = fog_height;
+}
+
+
+#version 120
+
+uniform sampler2D sampler;
+uniform sampler2D sky_sampler;
+uniform float timer;
+uniform float daylight;
+uniform int ortho;
+
+varying vec2 fragment_uv;
+varying float fragment_ao;
+varying float fragment_light;
+varying float fog_factor;
+varying float fog_height;
+varying float diffuse;
+
+const float pi = 3.14159265;
+
+void main() {
+    vec3 color = vec3(texture2D(sampler, fragment_uv));
+    if (color == vec3(1.0, 0.0, 1.0)) {
+        discard;
+    }
+    bool cloud = color == vec3(1.0, 1.0, 1.0);
+    if (cloud && bool(ortho)) {
+        discard;
+    }
+    float df = cloud ? 1.0 - diffuse * 0.2 : diffuse;
+    float ao = cloud ? 1.0 - (1.0 - fragment_ao) * 0.2 : fragment_ao;
+    ao = min(1.0, ao + fragment_light);
+    df = min(1.0, df + fragment_light);
+    float value = min(1.0, daylight + fragment_light);
+    vec3 light_color = vec3(value * 0.3 + 0.2);
+    vec3 ambient = vec3(value * 0.3 + 0.2);
+    vec3 light = ambient + light_color * df;
+    color = clamp(color * light * ao, vec3(0.0), vec3(1.0));
+    vec3 sky_color = vec3(texture2D(sky_sampler, vec2(timer, fog_height)));
+    color = mix(color, sky_color, fog_factor);
+    gl_FragColor = vec4(color, 1.0);
+}
+
+void block_fs(float* fs_input, Shader_Builtins* builtins, void* uniforms)
+{
+
+
+
+
+
+
+}
